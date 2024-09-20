aa4ebe935f4b2e5024ed1fc2133bac3db412145c
Raezil <kamil.mazurek319@gmail.com>
2024-09-20 20:38:23 +0200

    feat: new requests

--- main.go
+++ main.go
@@ -23,8 +23,16 @@ func Latest(currency string) string {
 	return UrlWithAPIKey() + "/latest/" + currency
 }
 
-func Exchange(left string, right string) string {
-	return UrlWithAPIKey() + "/pair/" + left + "/" + right
+func Exchange(from string, to string) string {
+	return UrlWithAPIKey() + "/pair/" + from + "/" + to
+}
+
+func EnrichedData(from string, to string) string {
+	return UrlWithAPIKey() + "/enriched/" + from + "/" + to
+}
+
+func History(currency, year, month, day string) string {
+	return UrlWithAPIKey() + "/history/" + currency + "/" + year + "/" + month + "/" + day
 }
 
 func Request(url string) ([]byte, error) {
@@ -61,9 +69,38 @@ func GetExchangedRequest(from, to string) map[string]interface{} {
 	return jsonMap
 }
 
+func GetEnrichedData(from, to string) map[string]interface{} {
+	url := EnrichedData(from, to)
+	body, err := Request(url)
+	if err != nil {
+		panic(err)
+	}
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(string(body)), &jsonMap)
+	return jsonMap
+}
+
+func GetHistory(currency, year, month, day string) map[string]interface{} {
+	url := History(currency, year, month, day)
+	body, err := Request(url)
+	if err != nil {
+		panic(err)
+	}
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(string(body)), &jsonMap)
+	return jsonMap
+}
+
 type CurrencyExchangeArgs struct {
 	From string
 	To   string
+}
+
+type CurrencyHistoryArgs struct {
+	Currency string
+	Year     string
+	Month    string
+	Day      string
 }
 
 type CurrencyExchangeListArgs struct {
@@ -83,6 +120,16 @@ func (h *CurrencyService) Latest(r *http.Request, args *CurrencyExchangeListArgs
 
 func (h *CurrencyService) Exchange(r *http.Request, args *CurrencyExchangeArgs, reply *CurrencyReply) error {
 	reply.Message = GetExchangedRequest(args.From, args.To)
+	return nil
+}
+
+func (h *CurrencyService) EnrichedData(r *http.Request, args *CurrencyExchangeArgs, reply *CurrencyReply) error {
+	reply.Message = GetEnrichedData(args.From, args.To)
+	return nil
+}
+
+func (h *CurrencyService) History(r *http.Request, args *CurrencyHistoryArgs, reply *CurrencyReply) error {
+	reply.Message = GetHistory(args.Currency, args.Year, args.Month, args.Day)
 	return nil
 }
 
